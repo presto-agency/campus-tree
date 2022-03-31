9a493364157df7088357eb2d6b1f757656d1ec13
artemats <artem.matseruk@gmail.com>
2022-03-31 10:31:12 +0300

    feat: (General) setup datepicker

---
+++ src/js/common/datepicker/initDatepicker.js
@@ -0,0 +1,10 @@
+import { Datepicker } from 'vanillajs-datepicker';
+
+export const initDatepicker = () => {
+	const inputs = document.querySelectorAll('.input-datepicker');
+	for (let i = 0; i < inputs.length; i++) {
+		const datepicker = new Datepicker(inputs[i], {
+
+		});
+	}
+};

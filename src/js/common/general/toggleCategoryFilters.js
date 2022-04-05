24303a058161f9523906fa0fef94cf0322215b57
artemats <artem.matseruk@gmail.com>
2022-04-05 23:42:51 +0300

    feat: (Category page) add tablet, phone versions

---
+++ src/js/common/general/toggleCategoryFilters.js
@@ -0,0 +1,26 @@
+import Tween from 'gsap';
+
+export const toggleCategoryFilters = () => {
+	const toggleTriggers = document.querySelectorAll('.toggle-filters');
+	for (let i = 0; i < toggleTriggers.length; i++) {
+		toggleTriggers[i].addEventListener('click', () => {
+			let filtersId = toggleTriggers[i].dataset.filterId;
+			const triggerFilter = document.querySelector(filtersId);
+			if (triggerFilter.classList.contains('is-visible')) {
+				Tween.set(triggerFilter, {
+					display: 'none',
+					onComplete: () => {
+						triggerFilter.classList.remove('is-visible');
+					},
+				});
+			} else {
+				Tween.set(triggerFilter, {
+					display: 'block',
+					onComplete: () => {
+						triggerFilter.classList.add('is-visible');
+					}
+				})
+			}
+		});
+	}
+};

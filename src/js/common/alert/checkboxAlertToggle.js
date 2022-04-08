6235b557ce1bdebb1f1f5e72e2113130444eb6a0
artemats <artem.matseruk@gmail.com>
2022-04-08 17:46:33 +0300

    update: (General) update pagination router transition

--- src/js/common/alert/checkboxAlertToggle.js
+++ src/js/common/alert/checkboxAlertToggle.js
@@ -5,26 +5,16 @@ export const checkboxAlertToggle = () => {
 	const boxes = document.querySelectorAll('[data-alert-trigger]');
 	for (let i = 0; i < boxes.length; i++) {
 		const alertTriggerId = boxes[i].dataset.alertTrigger;
-		const alertTriggerLabel = boxes[i].dataset.alertLabel;
-		const alertTriggerLabelSingle = boxes[i].dataset.alertLabelSingle;
 		const checkboxInputs = boxes[i].querySelectorAll('input[type="checkbox"]');
 		const triggerInput = document.querySelector(`[data-alert-checkbox="${alertTriggerId}"]`);
 
 		if (!!triggerInput) {
 			const triggerInputTitle = triggerInput.parentNode.querySelector('.input-checkbox-title');
-			const tooltip = triggerInput.parentNode.parentNode;
-			const tooltipDefaultLabel = tooltip.dataset.defaultLabel;
-			const tooltipActiveLabel = tooltip.dataset.activeLabel;
 
 			for (let j = 0; j < checkboxInputs.length; j++) {
 				checkboxInputs[j].addEventListener('change', () => {
 					triggerInput.checked = false;
-					/*
-					Update label
-					 */
-					triggerInputTitle.innerHTML = `${getChecked(checkboxInputs)} ${getChecked(checkboxInputs) > 1
-						? alertTriggerLabel
-						: alertTriggerLabelSingle}`;
+					updateLabel(boxes[i], triggerInputTitle, checkboxInputs, triggerInput);
 				});
 			}
 
@@ -36,20 +26,7 @@ export const checkboxAlertToggle = () => {
 					for (let j = 0; j < checkboxInputs.length; j++) {
 						checkboxInputs[j].checked = triggerInput.checked;
 					}
-					/*
-					Update label
-					 */
-					triggerInputTitle.innerHTML = `${getChecked(checkboxInputs)} ${getChecked(checkboxInputs) > 1
-						? alertTriggerLabel
-						: alertTriggerLabelSingle}`;
-
-					/*
-					Update trigger tooltip label
-					 */
-					const tooltipLabel = tooltip.querySelector('.tooltip-body-item');
-					triggerInput.checked
-						? tooltipLabel.innerHTML = tooltipActiveLabel
-						: tooltipLabel.innerHTML = tooltipDefaultLabel;
+					updateLabel(boxes[i], triggerInputTitle, checkboxInputs, triggerInput);
 				});
 			}
 		}
@@ -62,4 +39,33 @@ const getChecked = (checkboxInputs) => {
 		checkboxInputs[i].checked ? checked.push(checkboxInputs[i]) : null;
 	}
 	return checked.length;
+}
+
+const updateLabel = (box, triggerInputTitle, checkboxInputs, triggerInput) => {
+
+	const alertTriggerLabel = box.dataset.alertLabel;
+	const alertTriggerLabelSingle = box.dataset.alertLabelSingle;
+	const tooltip = triggerInput.parentNode.parentNode;
+	const tooltipDefaultLabel = tooltip.dataset.defaultLabel;
+	const tooltipActiveLabel = tooltip.dataset.activeLabel;
+	const tooltipLabel = tooltip.querySelector('.tooltip-body-item');
+	const alert = document.querySelector('.alert');
+
+	/*
+	Update label
+	 */
+	triggerInputTitle.innerHTML = `${getChecked(checkboxInputs)} ${getChecked(checkboxInputs) > 1
+		? alertTriggerLabel
+		: alertTriggerLabelSingle}`;
+
+	/*
+	Update trigger tooltip label
+	 */
+	triggerInput.checked
+		? tooltipLabel.innerHTML = tooltipActiveLabel
+		: tooltipLabel.innerHTML = tooltipDefaultLabel;
+
+	if (!!alert) {
+		getChecked(checkboxInputs) >= 1 ? alert.classList.add('is-active') : alert.classList.remove('is-active');
+	}
 }

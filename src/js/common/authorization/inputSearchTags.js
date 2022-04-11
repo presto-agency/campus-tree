8a5228628106b08d51f29a814465611ecf85bf1e
Anton K <kamarhin_anton@ukr.net>
2022-04-11 17:51:00 +0300

    feat: add input-search-tags

---
+++ src/js/common/authorization/inputSearchTags.js
@@ -0,0 +1,66 @@
+export const inputSearchTags = () =>{
+    const input = document.querySelector('#objects');
+    const suggestions = document.querySelector('.input-tags-suggestions ul');
+    const objects = [];
+
+    let clubs = document.querySelectorAll('.input-tags-suggestions>ul>li');
+    clubs.forEach(el => {
+        objects.push(el.innerHTML)
+    })
+
+    suggestions.innerHTML = '';
+
+    function search(str) {
+        let results = [];
+        const val = str.toLowerCase();
+
+        for (let i = 0; i < objects.length; i++) {
+            if (objects[i].toLowerCase().indexOf(val) > -1) {
+                results.push(objects[i]);
+            }
+        }
+
+        return results;
+    }
+
+    function searchHandler(e) {
+        const inputVal = e.currentTarget.value;
+        let results = [];
+        if (inputVal.length > 0) {
+            results = search(inputVal);
+        }
+        showSuggestions(results, inputVal);
+    }
+
+    function showSuggestions(results, inputVal) {
+
+        suggestions.innerHTML = '';
+
+        if (results.length > 0) {
+            for (let i = 0; i < results.length; i++) {
+                let item = results[i];
+                const match = item.match(new RegExp(inputVal, 'i'));
+                item = item.replace(match[0], `<strong>${match[0]}</strong>`);
+                suggestions.innerHTML += `<li>${item}</li>`;
+            }
+            suggestions.classList.add('has-suggestions');
+        } else {
+            results = [];
+            suggestions.innerHTML = '';
+            suggestions.classList.remove('has-suggestions');
+        }
+    }
+
+    function useSuggestion(e) {
+        let newEl = document.createElement('li');
+        let result = document.querySelector('.input-tags-results');
+        newEl.innerHTML =  `<span></span>${e.target.innerText}`;
+        result.appendChild(newEl);
+        suggestions.innerHTML = '';
+        suggestions.classList.remove('has-suggestions');
+        input.value = '';
+    }
+
+    input.addEventListener('keyup', searchHandler);
+    suggestions.addEventListener('click', useSuggestion);
+};

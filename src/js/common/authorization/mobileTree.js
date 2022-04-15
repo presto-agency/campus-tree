6f5ddda82ac220e475611aa8f9fbc5beb863a30d
Anton K <kamarhin_anton@ukr.net>
2022-04-15 17:26:33 +0300

    fix: js animation tree func

--- src/js/common/authorization/mobileTree.js
+++ src/js/common/authorization/mobileTree.js
@@ -1,18 +1,20 @@
 export const mobileTree = () => {
-    let btns = document.querySelectorAll('.trigger-mob');
-    let animationObj = document.querySelector('.sign-in');
-    let addActive = () => {
-        animationObj.classList.add('active');
-    }
-    let removeActive = () => {
-        animationObj.classList.remove('active');
-    }
-    addActive();
-    setTimeout(removeActive, 2500);
-    btns.forEach(btn => {
-        btn.onclick = () => {
-            addActive();
-            setTimeout(removeActive, 2500);
+    if (window.screen.width < 768){
+        let btns = document.querySelectorAll('.trigger-mob');
+        let animationObj = document.querySelector('.sign-in');
+        let addActive = () => {
+            animationObj.classList.add('active');
         }
-    })
+        let removeActive = () => {
+            animationObj.classList.remove('active');
+        }
+        addActive();
+        setTimeout(removeActive, 2500);
+        btns.forEach(btn => {
+            btn.onclick = () => {
+                addActive();
+                setTimeout(removeActive, 2500);
+            }
+        })
+    }
 }
